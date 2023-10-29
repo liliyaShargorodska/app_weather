3d5ce7c9c1b520f10005fc345a7856585a15383e
Liliia Sharhorodska <lilya.osipchuk@gmail.com>
2023-10-29 17:13:33 +0200

    add tests for 'getCurrentWeather'

--- command/current.test.js
+++ command/current.test.js
@@ -1,5 +1,5 @@
-import test, { describe } from "node:test";
-import { validate } from "./current.js";
+import test, { afterEach, describe, mock } from "node:test";
+import { getCurrentWeather, validate } from "./current.js";
 import assert from "node:assert/strict";
 
 describe("validate()", () => {
@@ -63,5 +63,93 @@ describe("validate()", () => {
     assert.throws(() => validate({ lat: "49.2328", long: "181" }), {
       message: "Longitude should be a number between -180 and 180",
     });
+  });
+});
+
+describe("getCurrentWeather()", () => {
+  let weatherProvider = {
+    async getCurrentWeatherByCoordinates() {},
+  };
+  let geocodeProvider = {
+    async getCoordinatesByCityName() {},
+  };
+
+  afterEach(() => {
+    mock.restoreAll();
+  });
+
+  test("should return weather by coordinates", async () => {
+    const expectedTime = new Date();
+    const expectedTemperature = 21;
+    const expectedLat = "49.2328";
+    const expectedLong = "28.4816";
+
+    const mockCurrentWeather = mock.method(
+      weatherProvider,
+      "getCurrentWeatherByCoordinates",
+      async () => ({
+        temperature: expectedTemperature,
+        time: expectedTime,
+        lat: expectedLat,
+        long: expectedLong,
+      }),
+    );
+
+    const logMock = mock.method(console, "log", () => {}, { times: 1 });
+
+    await getCurrentWeather(weatherProvider, geocodeProvider, {
+      lat: expectedLat,
+      long: expectedLong,
+    });
+
+    assert.equal(logMock.mock.calls.length, 1);
+    assert.equal(
+      logMock.mock.calls[0].arguments[0],
+      `Current weather in ${expectedLat} ${expectedLong}: ${expectedTemperature} °C (time: ${expectedTime})`,
+    );
+
+    assert.equal(mockCurrentWeather.mock.calls.length, 1);
+    assert.equal(mockCurrentWeather.mock.calls[0].arguments[0], expectedLat);
+    assert.equal(mockCurrentWeather.mock.calls[0].arguments[1], expectedLong);
+  });
+
+  test("should return weather by city name", async () => {
+    const expectedTime = new Date();
+    const expectedTemperature = 21;
+    const expectedLat = "49.2328";
+    const expectedLong = "28.4816";
+    const expectedName = "Vinnytsia";
+
+    const mockCurrentWeather = mock.method(
+      weatherProvider,
+      "getCurrentWeatherByCoordinates",
+      async () => ({
+        temperature: expectedTemperature,
+        time: expectedTime,
+        city: expectedName,
+      }),
+    );
+
+    mock.method(geocodeProvider, "getCoordinatesByCityName", async () => ({
+      name: expectedName,
+      latitude: expectedLat,
+      longitude: expectedLong,
+    }));
+
+    const logMock = mock.method(console, "log", () => {}, { times: 1 });
+
+    await getCurrentWeather(weatherProvider, geocodeProvider, {
+      city: "Vinnyt",
+    });
+
+    assert.equal(logMock.mock.calls.length, 1);
+    assert.equal(
+      logMock.mock.calls[0].arguments[0],
+      `Current weather in ${expectedName}: ${expectedTemperature} °C (time: ${expectedTime})`,
+    );
+
+    assert.equal(mockCurrentWeather.mock.calls.length, 1);
+    assert.equal(mockCurrentWeather.mock.calls[0].arguments[0], expectedLat);
+    assert.equal(mockCurrentWeather.mock.calls[0].arguments[1], expectedLong);
   });
 });
